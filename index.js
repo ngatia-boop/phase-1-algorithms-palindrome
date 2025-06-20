b3dce436eb2bbbfdbef7526e31cc029fd94616d3
ann ngatia <annngatia94@gmail.com>
2025-06-20 10:16:14 +0300

    algorithmic problem solving

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+ //step 1: reverse the string 
+ const reversedWord = word.split('').reverse().join('');
+
+ //step 2: compare reversed word with the original 
+ return word === reversedWord;
 }
 
+// test cases
+console.log(isPalindrome("madam")); //true
+console.log(isPalindrome("robot"));     // false
+console.log(isPalindrome("racecar"));   // true
+console.log(isPalindrome("a"));         // true
+console.log(isPalindrome("ab"));        // false
+console.log(isPalindrome("abba"));      // true
 /* 
   Add your pseudocode here
+1. Define a function isPalindrome that takes a string as an argument.
+2. Reverse the string.
+3. Compare the reversed string to the original string.
+4. If they are the same, return true.
+5. If not, return false.
+
 */
 
 /*
